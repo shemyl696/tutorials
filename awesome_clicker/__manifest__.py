79383ebf22f75578b593079e57fd1ad23045f752
shemyl696 <kirared199@gmail.com>
2025-11-10 12:31:45 +0300

    Update __manifest__.py

--- awesome_clicker/__manifest__.py
+++ awesome_clicker/__manifest__.py
@@ -7,7 +7,7 @@
     """,
 
     'description': """
-        Starting module for "Master the Odoo web framework, chapter 1: Build a Clicker game"
+        Starting module for "Master the Odoo web framework, chapter 2: Build a Clicker game"
     """,
 
     'author': "Odoo",
